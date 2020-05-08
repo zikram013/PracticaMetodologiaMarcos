b7e16d5b0b2db355421f4abba9c6fb06b2201dff
zikram013 <hernandezsanchezmarcos@gmail.com>
2020-05-08 22:21:35 +0200

    fix testing.

--- P2Metodologia/Test/com/company/Users/ManagerUsuarioTest.java
+++ P2Metodologia/Test/com/company/Users/ManagerUsuarioTest.java
@@ -10,33 +10,34 @@ public class ManagerUsuarioTest {
     @Test
     public void crearUsuario() {
         Usuario usuario=new Usuario("prueba","prueba","prueba","prueba","prueba","alumno");
-        assertTrue(managerUsuario.getListaDeUsuarios().add(usuario));
+        managerUsuario.crearUsuario(usuario);
+        assertEquals("existe Usuario",usuario,managerUsuario.usuarioExisteSistema(usuario.getCorreo(),usuario.getContrasena()));
     }
 
     @Test
     public void encontradoRolProfesor() {
         Usuario usuario=new Usuario("prueba","prueba","prueba","prueba","prueba","profesor");
-        String rol=usuario.getRol();
+        managerUsuario.crearUsuario(usuario);
         assertTrue(usuario.getRol()=="profesor");
     }
 
     @Test
     public void encontradoRolAdmin() {
         Usuario usuario=new Usuario("prueba","prueba","prueba","prueba","prueba","admin");
-        String rol=usuario.getRol();
+        managerUsuario.crearUsuario(usuario);
         assertTrue(usuario.getRol()=="admin");
     }
 
     @Test
     public void encontradoRolAlumno() {
         Usuario usuario=new Usuario("prueba","prueba","prueba","prueba","prueba","alumno");
-        String rol=usuario.getRol();
-        assertTrue(usuario.getRol()=="alumno");
+        managerUsuario.crearUsuario(usuario);
+        assertTrue(managerUsuario.encontradoRolAlumno(usuario));
     }
 
     @Test
     public void usuarioExisteSistema() {
         Usuario usuario=new Usuario("prueba","prueba","prueba","prueba","prueba","profesor");
-        assertEquals("Usuario no encontrado", false, managerUsuario.getListaDeUsuarios().contains(usuario));
+        assertEquals("Usuario no encontrado", false, managerUsuario.usuarioRegistrado(usuario));
     }
 }
